b9985b55f05be97deb8b9171db5f9778a4c9b261
Dhinsa <152815519+dhinsa1@users.noreply.github.com>
2023-12-03 16:16:51 -0500

    Add files via upload

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Vikram Dhinsa on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
